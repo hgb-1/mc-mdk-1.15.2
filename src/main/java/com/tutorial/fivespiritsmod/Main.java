2e4c4cafc5a816c7f361148b8725f6efa55133f1
hgb-1 <2017455452@qq.com>
2021-04-12 14:15:15 +0800

    方块，合成表，燃烧表

---
+++ src/main/java/com/tutorial/fivespiritsmod/Main.java
@@ -0,0 +1,25 @@
+package com.tutorial.fivespiritsmod;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod("spirits")
+public class Main {
+    // Directly reference a log4j logger.
+    public static final Logger LOGGER = LogManager.getLogger();
+
+    public Main() {
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event)
+    {
+    }
+
+}
